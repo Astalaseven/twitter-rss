6627ae75f561817a63451e114430ee5e8f1c7ac2
Gatien Bovyn <gatien.bovyn@gmail>
2013-06-19 11:25:57 +0200

    code refactored by C4ptainCrunch

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 import time
 import twitter_rss
@@ -6,44 +9,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Goto domain.com/twitterusername.xml"
+    return "Goto domain.com/twitterusername.xml"
 
 @app.route('/<username>.xml')
 def twitter_to_xml(username):
-
-	try:
-		write = True
-
-		with open('accounts.txt', 'r') as check_accounts:
-			check = check_accounts.read()
-			if username in check:
-				write = False
-		check_accounts.close()
-	except IOError:
-		print 'Accounts.txt could not be read'
-
-	try:
-		if write:
-			with open('accounts.txt', 'a') as usernames:
-				usernames.write(username + '\n')
-			usernames.close()
-	except IOError:
-		return 'Accounts.txt could not be written'
-
-	twitter_rss.TwitterToRss(username)
-
-		# print 'Waiting 10 seconds before feed is generated...'
-
-	try:
-		with open(username + '.xml', 'r') as content_file:
-			content = content_file.read()
-		content_file.close()
-
-	except IOError:
-		return 'File ' + username + '.xml could not be read'
-
-	return content 
+    tweets = twitter_rss.UserTweetGetter(username)
+    return tweets.to_rss()
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+    app.run(debug=True)
