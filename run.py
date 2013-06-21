15e5e2a8ff41b5dfc189a3988e0a7e33cbad3a8e
Gatien Bovyn <gatien.bovyn@gmail>
2013-06-21 12:21:37 +0200

    catch KeyboardInterrupt

--- run.py
+++ run.py
@@ -6,20 +6,23 @@ import subprocess
 p = subprocess.Popen(['/usr/bin/python2', 'server.py'])
 
 # Update the feeds
-while 1:
-	print 'Updating ALL THE FEEDS!'
-	try:
-		with open('user/user.txt', 'r') as usernames:
-			for user in usernames:
-				twitter_rss.UserTweetGetter(user)
-		usernames.close()
+try:
+	while 1:
+		print 'Updating ALL THE FEEDS!'
+		try:
+			with open('user/user.txt', 'r') as usernames:
+				for user in usernames:
+					twitter_rss.UserTweetGetter(user)
+			usernames.close()
 
-		with open('htag/htag.txt', 'r') as hashtags:
-			for htag in hashtags:
-				twitter_rss.HashtagTweetGetter(user)
-		hashtags.close()
-	except IOError:
-		print 'File could not be read'
-	time.sleep(600)
+			with open('htag/htag.txt', 'r') as hashtags:
+				for htag in hashtags:
+					twitter_rss.HashtagTweetGetter(user)
+			hashtags.close()
+		except IOError:
+			print 'File could not be read'
+		time.sleep(600)
 
-p.kill() # kill the subprocess
+	p.kill() # kill the subprocess
+except (KeyboardInterrupt, SystemExit):
+    print '\nKeyboardInterrupt catched -- Finishing program.'
