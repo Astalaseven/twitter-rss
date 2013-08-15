e1b2c80d87e333ba82f6f3eb7185e4f2b19dbeff
Astalaseven <gatien.bovyn@gmail.com>
2013-08-15 17:13:49 +0200

    replace last tweet checking with cache system
    based on the file modified date

--- server.py
+++ server.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask, render_template, request, redirect, url_for
+import os
+import arrow
 import config
 import re
 import requests
@@ -28,7 +30,7 @@ def handle_htag():
 
 @app.route('/<path>/<feed>.xml')
 def feed_to_xml(feed, path):
-    if check_if_new_tweet(feed, path) is False:
+    if check_cache_timeout(feed, path) is False:
         error = serve_from_cache(feed, path)
     else:
         try:
@@ -44,47 +46,16 @@ def feed_to_xml(feed, path):
         save_feed_for_updating(tweets, feed, path)
     return error
 
-def find_new_first_id(feed, path):
-    if path == 'user':
-        url = "https://mobile.twitter.com/{0}"
+def check_cache_timeout(feed, path):
+    xml_file = config.XML_DIR + path + '/' + feed + '.xml'
+    time_now = arrow.utcnow()
+    if os.path.isfile(xml_file):
+        time_modified_file = arrow.get(os.path.getmtime(xml_file)).replace(minutes=+config.CACHE)
+        print('{0} seconds left before next updating'.format((time_modified_file - time_now).seconds))
+        result = time_now > time_modified_file
     else:
-        url = "https://mobile.twitter.com/search?q=%23{0}"
-    content = requests.get(url.format(feed))
-    content = content.text.encode('utf-8')
-    try:
-        new_first_id = re.findall(r'data-id="(\d+)"', str(content))[0]
-    except IndexError:
-        new_first_id = "42"
-    return new_first_id
-
-def find_old_first_id(txt_file, feed):
-    old_first_id = ''
-    try:
-        with open(txt_file, 'r') as f:
-            content = f.read()
-            old_first_id = re.findall("{0} -- (\d+)".format(feed), content)[0]
-    except IndexError:
-        old_first_id = "1337"
-    return old_first_id
-
-def update_id_in_txt_file(txt_file, feed, new_first_id):
-    with open(txt_file, 'r') as f:
-            content = f.read()
-    with open(txt_file, 'w') as m:
-        update = re.sub("{0}.*".format(feed), "{0} -- {1}".format(feed, new_first_id), content)
-        m.write(update)
-
-def check_if_new_tweet(feed, path):
-    print('Checking if new tweet for {0}...'.format(feed))
-    update = True
-    txt_file = config.XML_DIR + path + '/' + path + '.txt'
-    new_first_id = find_new_first_id(feed, path)
-    old_first_id = find_old_first_id(txt_file, feed)
-    if new_first_id == old_first_id:
-        update = False
-    else:
-        update_id_in_txt_file(txt_file, feed, new_first_id)
-    return update
+        result = True
+    return result
 
 def serve_from_cache(feed, path):
     error = ''
@@ -117,11 +88,9 @@ def write_data_to_file(tweets, feed, path):
     return error
 
 def save_feed_for_updating(tweets, feed, path):
-    new_first_id = find_new_first_id(feed, path)
     try:
         with open(config.XML_DIR + path + '/' + path + '.txt', 'a') as save:
-            save.write(feed + ' -- ' + new_first_id + '\n')
-
+            save.write(feed + '\n')
     except IOError:
         print 'Could not save ' + feed + ' in ' + path
 
